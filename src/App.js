f4325b22db30de3d939686f1c97c029b5e440ab2
Paul Miller <paul@battlestardigital.com>
2019-06-19 08:16:41 -0400

    Added face-detection box

--- src/App.js
+++ src/App.js
@@ -41,7 +41,16 @@ class App extends Component {
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }    
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -54,8 +63,8 @@ class App extends Component {
       .predict(
           Clarifai.FACE_DETECT_MODEL, 
           this.state.input)
-      .then(response => this.calculateFaceLocation(response))
-      .catch(err => console.log(err))    
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -70,7 +79,7 @@ class App extends Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );    
   }
