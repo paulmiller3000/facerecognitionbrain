dc517a991dfae8173bdc71c3cf68d620f520f65d
Paul Miller <paul@battlestardigital.com>
2019-06-19 06:01:34 -0400

    Updated API call to recognize face, console log height and width

--- src/App.js
+++ src/App.js
@@ -31,8 +31,17 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
   }
 
   onInputChange = (event) => {
@@ -43,15 +52,10 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-          Clarifai.COLOR_MODEL, 
+          Clarifai.FACE_DETECT_MODEL, 
           this.state.input)
-      .then(function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // error
-      }
-    );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err))    
   }
 
   render() {
