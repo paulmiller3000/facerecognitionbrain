9b19c6c5e299b8c30cd60f03b12269b019d6fe2e
Paul Miller <paul@battlestardigital.com>
2019-07-16 16:54:29 -0400

    Removed forced error from Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {	
-	if (true) {
-		throw new Error('Ruh roh');
-	}
 	if(isSignedIn) {
 		return (
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
